4c0196475b9857379b9ef9ae5dfda259fe60b017
Ryan Ferreira <125921102+PhyxionSoft@users.noreply.github.com>
2023-10-12 12:33:19 -0300

    Update v0.1.5

--- project/run.js
+++ project/run.js
@@ -11,3 +11,38 @@ function ToggleMapConfigMenu() {
     var menu = document.querySelector('.MapConfigBackground');
     menu.style.width = menu.style.width == '30%' ? '0' : '30%';
 }
+
+function AdicionarNovoObjeto() {
+    var NovoObjeto = document.createElement("div");
+    NovoObjeto.className = "NovoObjeto";
+    NovoObjeto.textContent = "Objeto " + (document.querySelectorAll('.NovoObjeto').length + 1);
+
+    var ObjetosExistentes = document.querySelectorAll('.NovoObjeto');
+    if (ObjetosExistentes.length > 0) {
+        var UltimoObjeto = ObjetosExistentes[ObjetosExistentes
+            .length - 1];
+        NovoObjeto.style.left = (UltimoObjeto.offsetLeft + UltimoObjeto.offsetWidth + 48) + "px";
+    } else {
+        NovoObjeto.style.left = "15px";
+    }
+
+    document.querySelector('.CodingArea').appendChild(NovoObjeto);
+
+    var BotaoAdicionarBloco = document.createElement("button");
+    BotaoAdicionarBloco.textContent = "Adicionar Bloco";
+    BotaoAdicionarBloco.className = "AdicionarBlocoButton";
+    BotaoAdicionarBloco.onclick = AdicionarBloco; 
+
+    NovoObjeto.appendChild(BotaoAdicionarBloco);
+}
+
+
+
+function AdicionarBloco() {
+    var NovoBloco = document.createElement("div");
+    NovoBloco.className = "NovoBloco";
+    NovoBloco.textContent = "Novo Bloco";
+
+    document.querySelector('.NovoObjeto').appendChild(NovoBloco);
+}
+
--- script.js
+++ script.js
@@ -2,4 +2,3 @@ function toggleMenu() {
     var menu = document.querySelector('.TopMenu');
     menu.style.width = menu.style.width == '17%' ? '0' : '17%';
 }
-
